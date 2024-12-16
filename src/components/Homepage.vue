48da1157587fe45143693d0b90a6aa1833d25da8
Long Nguyen <nguy4049@umn.edu>
2024-12-15 22:26:08 -0600

    add more stuffs

---
+++ src/components/Homepage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import PrimaryTemplate from "../templates/PrimaryTemplate.vue";
+</script>
+
+<template>
+  <PrimaryTemplate>
+    Hello world!!
+  </PrimaryTemplate>
+</template>
+
+<style scoped>
+
+</style>
